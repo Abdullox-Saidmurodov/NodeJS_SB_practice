455d0a40fe883d41ada4782d2f584711bc58342e
Abdullox Saidmurodov <a.saidmurodov@mail.ru>
2025-01-23 20:37:02 +0500

    12

---
+++ modules/cli.js
@@ -0,0 +1,15 @@
+// console.log(process.argv)
+
+const logger = () => {
+    const resp = {}
+
+    for(let i = 2; i < process.argv.length; i++) {
+        const arg = process.argv[i].split('=')
+        // console.log(arg)
+        resp[arg[0]] = arg[1] ? arg[1] : true
+    }
+
+    return resp
+}
+
+console.log(logger())
